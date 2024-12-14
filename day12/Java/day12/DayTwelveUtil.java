f4bb7f147858aa0140ee59c6d2f6ae93f9508d35
SipuliCodes <vepsalainensisu@gmail.com>
2024-12-14 12:46:33 +0200

    Day 12 puzzle2 solved with Java

--- day12/Java/day12/DayTwelveUtil.java
+++ day12/Java/day12/DayTwelveUtil.java
@@ -61,4 +61,127 @@ public class DayTwelveUtil {
         }
         return price;
     }
+
+    private static int calculateCorners(ArrayList<String> map, Point currentCoordinates, char symbol) {
+        int cornerAmount = 0;
+        int x = currentCoordinates.x;
+        int y = currentCoordinates.y;
+
+        if(x > 0 && y > 0 && map.get(y).charAt(x-1) == symbol && map.get(y-1).charAt(x) == symbol && map.get(y-1).charAt(x-1) != symbol) {
+            cornerAmount++;
+        }
+
+        if(x < map.getFirst().length() - 1 && y > 0 && map.get(y).charAt(x+1) == symbol && map.get(y-1).charAt(x) == symbol && map.get(y-1).charAt(x+1) != symbol) {
+            cornerAmount++;
+        }
+
+        if(x > 0 && y < map.size() - 1 && map.get(y).charAt(x-1) == symbol && map.get(y+1).charAt(x) == symbol && map.get(y+1).charAt(x-1) != symbol) {
+            cornerAmount++;
+        }
+
+        if(x < map.getFirst().length() - 1 && y < map.size() - 1 && map.get(y).charAt(x+1) == symbol && map.get(y+1).charAt(x) == symbol && map.get(y+1).charAt(x+1) != symbol) {
+            cornerAmount++;
+        }
+
+        if(x > 0 && y > 0 && map.get(y).charAt(x-1) != symbol && map.get(y-1).charAt(x) != symbol) {
+            cornerAmount++;
+        }
+
+        if(x < map.getFirst().length() - 1 && y > 0 && map.get(y).charAt(x+1) != symbol && map.get(y-1).charAt(x) != symbol ) {
+            cornerAmount++;
+        }
+
+        if(x > 0 && y < map.size() - 1 && map.get(y).charAt(x-1) != symbol && map.get(y+1).charAt(x) != symbol) {
+            cornerAmount++;
+        }
+
+        if(x < map.getFirst().length() - 1 && y < map.size() - 1 && map.get(y).charAt(x+1) != symbol && map.get(y+1).charAt(x) != symbol ) {
+            cornerAmount++;
+        }
+
+        if(x == 0 && y == 0 || x==0 && y == map.size() - 1 || x == map.getFirst().length() - 1 && y == 0 || x == map.getFirst().length() - 1 && y == map.size() - 1) {
+            cornerAmount++;
+        }
+
+        if(x == 0 && y > 0 && map.get(y - 1).charAt(x) != symbol){
+            cornerAmount++;
+        }
+        if(x == 0 && y < map.size() - 1 && map.get(y + 1).charAt(x) != symbol){
+            cornerAmount++;
+        }
+
+        if(x == map.getFirst().length() - 1 && y > 0 && map.get(y - 1).charAt(x) != symbol){
+            cornerAmount++;
+        }
+        if(x == map.getFirst().length() - 1 && y < map.size() - 1 && map.get(y + 1).charAt(x) != symbol){
+            cornerAmount++;
+        }
+
+        if(y == 0 && x > 0 && map.get(y).charAt(x-1) != symbol){
+            cornerAmount++;
+        }
+        if(y == 0 && x < map.getFirst().length() - 1 && map.get(y).charAt(x+1) != symbol){
+            cornerAmount++;
+        }
+
+        if(y == map.size() - 1 && x > 0 && map.get(y).charAt(x-1) != symbol){
+            cornerAmount++;
+        }
+        if(y == map.size() - 1 && x < map.getFirst().length() - 1 && map.get(y).charAt(x+1) != symbol){
+            cornerAmount++;
+        }
+
+        return cornerAmount;
+    }
+
+    private static void calculateAreaAndSideHelper(HashSet<Point> alreadyBeen, ArrayList<String> map, Point currentCoordinates, int[] areaAndSides, char symbol) {
+        if(alreadyBeen.contains(currentCoordinates)){
+            return;
+        } else {
+            areaAndSides[0] += 1;
+            alreadyBeen.add(currentCoordinates);
+        }
+        int x = currentCoordinates.x;
+        int y = currentCoordinates.y;
+
+        areaAndSides[1] += calculateCorners(map, currentCoordinates, symbol);
+
+        if( x > 0 && map.get(y).charAt(x - 1) == symbol) {
+            calculateAreaAndSideHelper(alreadyBeen, map, new Point(x-1, y), areaAndSides, symbol);
+        }
+
+        if( x < map.get(y).length() - 1 && map.get(y).charAt(x + 1) == symbol) {
+            calculateAreaAndSideHelper(alreadyBeen, map, new Point(x+1, y), areaAndSides, symbol);
+        }
+
+        if( y > 0 && map.get(y-1).charAt(x) == symbol) {
+            calculateAreaAndSideHelper(alreadyBeen, map, new Point(x, y-1), areaAndSides, symbol);
+        }
+
+        if( y < map.size() - 1 && map.get(y+1).charAt(x) == symbol) {
+            calculateAreaAndSideHelper(alreadyBeen, map, new Point(x, y+1), areaAndSides, symbol);
+        }
+
+    }
+    private static int[] calculateAreaAndSide(HashSet<Point> alreadyBeen, ArrayList<String> map, Point currentCoordinates, char symbol ) {
+        int[] areaAndSides = {0, 0};
+
+        calculateAreaAndSideHelper(alreadyBeen, map, currentCoordinates, areaAndSides, symbol);
+        return areaAndSides;
+    }
+    public static int calculateDiscountPrice(ArrayList<String> map) {
+        int price = 0;
+        HashSet<Point> alreadyBeen = new HashSet<Point>();
+
+        for(int y = 0; y < map.size(); y++) {
+            for(int x = 0; x < map.get(y).length(); x++) {
+                if(alreadyBeen.contains(new Point(x, y))){
+                    continue;
+                }
+                int[] areaAndSides = calculateAreaAndSide(alreadyBeen, map, new Point(x, y), map.get(y).charAt(x));
+                price += areaAndSides[0] * areaAndSides[1];
+            }
+        }
+        return price;
+    }
 }
